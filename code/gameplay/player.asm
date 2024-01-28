7b628aacebda879e5e89c20fa84108531ad31bef
Grzegorz 'Envy' Perzanowski <grzegorz@envysoftware.pl>
2024-01-28 13:59:30 +0100

    animation2

--- code/gameplay/player.asm
+++ code/gameplay/player.asm
@@ -1,6 +1,7 @@
 SECTION "PlayerVariables", WRAM0
-wPlayerPositionX:: dw
-wPlayerPositionY:: dw
+wPlayerPositionX:: db
+wPlayerPositionY:: db
+wPlayerDirection:: db; 0 => right, 1 => left
 
 section "Player", rom0
 
@@ -11,6 +12,7 @@ SetupPlayer:
 	ld [wPlayerPositionY], a
 	ld a, 0
 	ld [wPlayerScore], a
+	ld [wPlayerDirection], a
 	ld a, PLAYER_START_HP
 	ld [wPlayerHp], a
 	ret
@@ -18,11 +20,19 @@ SetupPlayer:
 UpdatePlayer:
 	call HandlePlayerInput
 
+	ld e, 0
+	ld a, [wPlayerDirection]
+	cp a, 1
+	jp nz, .updatePlayerDraw
+	ld a, 0
+	set 5, a
+	ld e, a
+
+.updatePlayerDraw:
 	ld b, 128 ;144
 	ld a, [wPlayerPositionX]
 	ld c, a
 	ld d, PLAYER_TILE
-	ld e, 0
 	call RenderSimpleSprite
 	ret
 
@@ -33,6 +43,8 @@ HandlePlayerInput:
 	and a, PADF_LEFT
 	jp z, .checkRight
 .left:
+	ld a, 1
+	ld [wPlayerDirection], a
 	ld a, [wPlayerPositionX]
 	dec a
 	cp a, PLAYER_MAX_LEFT
@@ -44,6 +56,8 @@ HandlePlayerInput:
 	and a, PADF_RIGHT
 	ret z
 .right:
+	ld a, 0
+	ld [wPlayerDirection], a
 	ld a, [wPlayerPositionX]
 	inc a
 	cp a, PLAYER_MAX_RIGHT
